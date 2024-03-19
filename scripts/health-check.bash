cd2fb15fcfe20758f1235c1995cd41a357bdbb1e
Alvis <uualvislu@gmail.com>
2024-03-19 17:37:03 +0800

    update health-check

--- scripts/health-check.bash
+++ scripts/health-check.bash
@@ -3,10 +3,20 @@
 #
 
 BE_SVC_URL=https://backend.dev.simpos.com.tw/
+DB_HEALTHZ=https://backend.dev.simpos.com.tw/healthz
 
-RESPONSE=$(curl -s -X GET "$BE_SVC_URL")
-echo $RESPONSE
-if [ "$RESPONSE" = "Hello World!" ]; then
-	echo 'get Hello World!'
-	exit 1
-fi
+BE_STATUS=$(curl -s -X GET "$BE_SVC_URL" | jq -r '.status')
+DB_STATUS=$(curl -s -X GET "$DB_HEALTHZ" | jq -r '.status')
+
+STATUSES=("$DB_STATUS" "$BE_STATUS")
+
+# 檢查每個狀態，如果其中一個不是 "true"，則輸出相應的錯誤消息
+for index in "${!STATUSES[@]}"; do
+	STATUS="${STATUSES[index]}"
+	if [ "$STATUS" != "ok" ]; then
+		echo ""
+		echo "Error: STATUSES[$index] is not ok"
+		echo ""
+		exit 1
+	fi
+done
